434be7db477307b2233e20ea57772f19e56a7198
Cahir <artur08062003@mail.ru>
2024-10-07 14:56:28 +0600

    test #6

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,14 @@
 import React from 'react';
 import MyDataTable from './components/user_table.tsx';
 import WebApp from '@twa-dev/sdk';
-import { MainButton } from '@twa-dev/sdk/react';
 
 export const App: React.FC = () => {
 
     // Определение класса темы на основе цветовой схемы
     const themeClass = WebApp.colorScheme === 'dark' ? 'dark' : 'light';
-    const user = WebApp.initDataUnsafe.user?.username;
-
-    const show = () => {
-        WebApp.showAlert(user ? user : 'undef')
-    }
+   
     return (
         <div className={`bg-${themeClass}-background text-${themeClass}-text`}>
-            <MainButton onClick={show} text='123'/>
             <MyDataTable />
         </div>
     );
